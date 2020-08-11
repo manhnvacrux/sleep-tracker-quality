9ee27c78e19776e48828a655fd745e3edb5a485d
manhnvhanu <manhnvhanu@outlook.com>
2020-08-12 02:47:15 +0700

    Transform button states

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -58,6 +58,16 @@ class SleepTrackerViewModel(
     val navigateToSleepNight: LiveData<SleepNight>
         get() = _navigateToSleepQuality
 
+    val startButtonVisible = Transformations.map(tonight) {
+        null == it
+    }
+    val stopButtonVisible = Transformations.map(tonight) {
+        null != it
+    }
+    val clearButtonVisible = Transformations.map(nights) {
+        it?.isNotEmpty()
+    }
+
     fun doneNavigating() {
         _navigateToSleepQuality.value = null
     }
